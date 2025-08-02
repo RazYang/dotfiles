14f7971a2b3d49252f2779ec58ab01ec0b4496ad
RazYang <xzzorz@gamil.com>
2025-08-02 12:31:18 +0800

    nix fmt

--- packages/default.nix
+++ packages/default.nix
@@ -1,8 +1,6 @@
 { inputs, pkgs }:
-let
-  callPackage = file: args: pkgs.callPackage file ({ inherit inputs; } // args);
-in
 {
-  nixvim = callPackage ./nixvim.nix { };
-  hm = inputs.home-manager.packages.x86_64-linux.home-manager;
+  inherit (pkgs) hello;
+  #nixvim = callPackage ./nixvim.nix { };
+  #hm = inputs.home-manager.packages.x86_64-linux.home-manager;
 }
