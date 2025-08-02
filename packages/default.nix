0cccf84414f626d012d88476471df9b4f7051580
RazYang <xzzorz@gamil.com>
2025-08-02 13:28:01 +0800

    fix

--- packages/default.nix
+++ packages/default.nix
@@ -1,6 +1,9 @@
-{ pkgs }:
+{ inputs, pkgs }:
+let
+  callPackage = file: args: pkgs.callPackage file ({ inherit inputs; } // args);
+in
 {
   inherit (pkgs) hello;
-  #nixvim = callPackage ./nixvim.nix { };
-  #hm = inputs.home-manager.packages.x86_64-linux.home-manager;
+  nixvim = callPackage ./nixvim.nix { };
+  hm = inputs.home-manager.packages.x86_64-linux.home-manager;
 }
