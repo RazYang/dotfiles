f65f35fe6ac5f79727b5f0325f6802ebb52d485a
RazYang <xzzorz@gmail.com>
2025-10-01 13:41:46 +0800

    merge playground

--- overlays/default.nix
+++ overlays/default.nix
@@ -5,14 +5,5 @@
   };
   default = _final: prev: {
     factorio = prev.callPackage ./factorio.nix { };
-
-    # example
-    hello = prev.hello.overrideAttrs (_oldAttrs: rec {
-      version = "2.12";
-      src = prev.fetchurl {
-        url = "mirror://gnu/hello/hello-${version}.tar.gz";
-        hash = "sha256-zwSvhtwIUmjF9EcPuuSbGK+8Iht4CWqrhC2TSna60Ks=";
-      };
-    });
   };
 }
