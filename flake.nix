a2afee0b3f4968d1a0eb7d21b1fb7e8d39524ca1
RazYang <xzzorz@gamil.com>
2025-12-10 18:19:57 +0800

    rewrite flake inputs

--- flake.nix
+++ flake.nix
@@ -19,34 +19,75 @@
   };
 
   inputs = {
-    flake-utils.url = "https://github.com/numtide/flake-utils/archive/11707dc.zip";
+    infuse = {
+      url = "git+https://codeberg.org/amjoseph/infuse.nix.git";
+      flake = false;
+    };
+
+    systems = {
+      url = "path:./systems.nix";
+      flake = false;
+    };
+
     nixpkgs.url = "git+https://mirrors.tuna.tsinghua.edu.cn/git/nixpkgs.git?ref=nixos-25.11&shallow=1";
     nixpkgs-lib.url = "git+https://mirrors.tuna.tsinghua.edu.cn/git/nixpkgs.git?ref=nixos-25.11&shallow=1";
-    home-manager.url = "https://github.com/nix-community/home-manager/archive/20561be.zip";
-    nix-darwin.url = "https://github.com/nix-darwin/nix-darwin/archive/688427b.zip";
-    nix-index-database.url = "https://github.com/nix-community/nix-index-database/archive/4194c58.zip";
-    nixvim.url = "https://github.com/nix-community/nixvim/archive/a9d0e06.zip";
     impermanence.url = "https://github.com/nix-community/impermanence/archive/4b3e914.zip";
-    treefmt-nix.url = "https://github.com/numtide/treefmt-nix/archive/5b4ee75.zip";
-    nixos-generators.url = "https://github.com/nix-community/nixos-generators/archive/032a187.zip";
-    flake-parts.url = "https://github.com/hercules-ci/flake-parts/archive/2cccadc.zip";
-    infuse.url = "git+https://codeberg.org/amjoseph/infuse.nix.git";
-    devshell.url = "https://github.com/numtide/devshell/archive/17ed8d9.zip";
-    infuse.flake = false;
-    systems.url = "path:./systems.nix";
-    systems.flake = false;
 
-    flake-utils.inputs.systems.follows = "nixpkgs";
-    home-manager.inputs.nixpkgs.follows = "nixpkgs";
-    nix-index-database.inputs.nixpkgs.follows = "nixpkgs";
-    nixvim.inputs.nixpkgs.follows = "nixpkgs";
-    nixvim.inputs.systems.follows = "systems";
-    nixvim.inputs.flake-parts.follows = "flake-parts";
-    flake-parts.inputs.nixpkgs-lib.follows = "nixpkgs-lib";
-    treefmt-nix.inputs.nixpkgs.follows = "nixpkgs";
-    nixos-generators.inputs.nixpkgs.follows = "nixpkgs";
-    nixos-generators.inputs.nixlib.follows = "nixpkgs-lib";
-    nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+    flake-utils = {
+      url = "https://github.com/numtide/flake-utils/archive/11707dc.zip";
+      inputs.systems.follows = "nixpkgs";
+    };
+
+    home-manager = {
+      url = "https://github.com/nix-community/home-manager/archive/20561be.zip";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    nix-darwin = {
+      url = "https://github.com/nix-darwin/nix-darwin/archive/688427b.zip";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    nix-index-database = {
+      url = "https://github.com/nix-community/nix-index-database/archive/4194c58.zip";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+      };
+    };
+
+    nixvim = {
+      url = "https://github.com/nix-community/nixvim/archive/a9d0e06.zip";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        systems.follows = "systems";
+        flake-parts.follows = "flake-parts";
+      };
+    };
+    treefmt-nix = {
+      url = "https://github.com/numtide/treefmt-nix/archive/5b4ee75.zip";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    nixos-generators = {
+      url = "https://github.com/nix-community/nixos-generators/archive/032a187.zip";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        nixlib.follows = "nixpkgs-lib";
+      };
+    };
+
+    flake-parts = {
+      url = "https://github.com/hercules-ci/flake-parts/archive/2cccadc.zip";
+      inputs.nixpkgs-lib.follows = "nixpkgs-lib";
+    };
+
+    services-flake.url = "https://github.com/juspay/services-flake/archive/8b6244f.zip";
+    process-compose-flake.url = "https://github.com/Platonic-Systems/process-compose-flake/archive/3667881.zip";
+
+    devshell = {
+      url = "https://github.com/numtide/devshell/archive/17ed8d9.zip";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs =
