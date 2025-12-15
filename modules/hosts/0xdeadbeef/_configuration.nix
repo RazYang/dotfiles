084674935d0d52e1eed72431baec586adbf242e7
RazYang <xzzorz@gmail.com>
2025-12-15 22:38:57 +0800

    nix-darwin host 0xdeadbeef config

--- modules/hosts/0xdeadbeef/_configuration.nix
+++ modules/hosts/0xdeadbeef/_configuration.nix
@@ -5,12 +5,27 @@
   ...
 }:
 {
+  users.users.yang.home = "/Users/yang";
   home-manager.extraSpecialArgs.inputs = inputs;
   home-manager.users.yang = {
     imports = users.yang.modules;
   };
+  nix = {
+    settings = {
+      experimental-features = [
+        "nix-command"
+        "flakes"
+        "pipe-operators"
+      ];
+      trusted-users = [
+        "yang"
+        "root"
+      ];
+    };
+    optimise.automatic = true;
+  };
 
   environment.enableAllTerminfo = true;
   environment.systemPackages = with pkgs; [ ];
-  system.stateVersion = "24.05";
+  system.stateVersion = 6;
 }
