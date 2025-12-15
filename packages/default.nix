0ca9a87dbdfc791dae6d4f9037fa8a78891185d9
RazYang <xzzorz@gamil.com>
2025-12-15 15:06:32 +0800

    pkgsCross support

--- packages/default.nix
+++ packages/default.nix
@@ -14,25 +14,40 @@
     }:
     let
       toFix = self: {
-        callPackage = lib.callPackageWith (
-          lib.mergeAttrsList [
-            pkgs
-            self.packages
-            {
-              inherit inputs infuse;
-              inherit (config.allModuleArgs) self' inputs' system;
-            }
-          ]
-        );
-        packages = (
-          lib.fileset.fileFilter ({ name, ... }: name == "package.nix") ./.
-          |> lib.fileset.toList
-          |> lib.map (path: {
-            name = builtins.dirOf path |> builtins.baseNameOf;
-            value = self.callPackage (import path) { };
-          })
-          |> lib.listToAttrs
-        );
+        callPackage =
+          pkgsArg:
+          lib.callPackageWith (
+            lib.mergeAttrsList [
+              pkgsArg
+              self.packages
+              {
+                inherit inputs infuse;
+                inherit (config.allModuleArgs) self' inputs' system;
+              }
+            ]
+          );
+        myPkgs =
+          callPackage:
+          (
+            lib.fileset.fileFilter ({ name, ... }: name == "package.nix") ./.
+            |> lib.fileset.toList
+            |> lib.map (path: {
+              name = builtins.dirOf path |> builtins.baseNameOf;
+              value = callPackage (import path) { };
+            })
+            |> lib.listToAttrs
+          );
+        packages = (self.myPkgs (self.callPackage pkgs)) // {
+          pkgsCross =
+            (pkgs.writeText "pkgsCross" "")
+            // (
+              lib.map (crossSystem: {
+                name = crossSystem;
+                value = self.myPkgs (self.callPackage pkgs.pkgsCross."${crossSystem}");
+              }) (lib.attrNames lib.systems.examples)
+              |> lib.listToAttrs
+            );
+        };
       };
     in
     {
