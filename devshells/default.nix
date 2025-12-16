d667c267491074eeee4c4a4f8b01814f1afd4a2e
RazYang <xzzorz@gamil.com>
2025-12-16 17:51:01 +0800

    add starship to devshell

--- devshells/default.nix
+++ devshells/default.nix
@@ -7,15 +7,28 @@
 {
   imports = [ inputs.devshell.flakeModule ];
   perSystem =
-    { config, ... }:
+    { config, pkgs, ... }:
+    let
+      starshipConfig = pkgs.writers.writeTOML "starship.toml" { };
+    in
     {
-      devshells =
+      devshells = (
         lib.fileset.fileFilter ({ name, ... }: name == "devshell.nix") ./.
         |> lib.fileset.toList
         |> lib.map (path: {
           name = builtins.dirOf path |> builtins.baseNameOf;
-          value = import path config.allModuleArgs;
+          value = {
+            imports = [ (import path config.allModuleArgs) ];
+            devshell = {
+              motd = "";
+              interactive.PS1.text = ''
+                export STARSHIP_CONFIG=${starshipConfig}
+                eval -- "''$(${pkgs.starship}/bin/starship init bash --print-full-init)"
+              '';
+            };
+          };
         })
-        |> lib.listToAttrs;
+        |> lib.listToAttrs
+      );
     };
 }
