eb7c1f585e89d913bc642616355035311b2ed701
RazYang <xzzorz@gamil.com>
2025-12-19 14:53:19 +0800

    add nix-direnv

--- modules/users/razyang.nix
+++ modules/users/razyang.nix
@@ -30,6 +30,7 @@ in
           programs = {
             direnv = {
               enable = true;
+              nix-direnv.enable = true;
               enableZshIntegration = true;
             };
           };
