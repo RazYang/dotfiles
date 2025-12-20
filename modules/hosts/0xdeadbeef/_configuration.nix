7cbc7b8417be3100b0bc79673af30d39f9ac6c49
RazYang <xzzorz@gmail.com>
2025-12-20 15:33:55 +0800

    update 0xdeadbeef config

--- modules/hosts/0xdeadbeef/_configuration.nix
+++ modules/hosts/0xdeadbeef/_configuration.nix
@@ -4,17 +4,71 @@
   users,
   ...
 }:
+let
+  primaryUser = "yang";
+in
 {
-  users.users.yang.home = "/Users/yang";
-  home-manager.users.yang = {
-    imports = users.yang.modules;
+  nix.settings.trusted-users = [ primaryUser ];
+  users.users."${primaryUser}".home = "/Users/${primaryUser}";
+  home-manager.users.yang.imports = users."${primaryUser}".modules;
+  services = {
+    yabai.enable = true;
+    skhd.enable = true;
   };
-  nix.settings.trusted-users = [
-    "root"
-    "razyang"
-  ];
 
   environment.enableAllTerminfo = true;
-  environment.systemPackages = with pkgs; [ ];
+  environment.systemPackages = with pkgs; [
+    xz
+    zip
+    gnutar
+    curl
+    wget
+    tree
+    gawk
+    gnused
+    gnugrep
+    findutils
+
+    ghostty-bin
+    dbeaver-bin
+    wireshark
+    drawio
+    imhex
+  ];
+
+  fonts.packages = with pkgs; [
+    nerd-fonts.iosevka-term
+    nerd-fonts.iosevka
+  ];
+
+  system.defaults = {
+    NSGlobalDomain = {
+      AppleShowAllFiles = true;
+      AppleShowScrollBars = "Automatic";
+      NSAutomaticWindowAnimationsEnabled = false;
+      NSStatusItemSelectionPadding = 1;
+      NSStatusItemSpacing = 1;
+      "com.apple.keyboard.fnState" = false;
+    };
+    controlcenter.BatteryShowPercentage = true;
+    dock = {
+      autohide = true;
+      tilesize = 48;
+    };
+    finder = {
+      AppleShowAllFiles = true;
+      AppleShowAllExtensions = true;
+      FXEnableExtensionChangeWarning = false;
+      FXRemoveOldTrashItems = true;
+      ShowPathbar = true;
+      FXPreferredViewStyle = "Nlsv";
+      _FXSortFoldersFirst = true;
+    };
+  };
+  system.keyboard = {
+    enableKeyMapping = true;
+    remapCapsLockToControl = true;
+  };
+  system.primaryUser = primaryUser;
   system.stateVersion = 6;
 }
