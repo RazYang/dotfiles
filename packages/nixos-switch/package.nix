602052dc528b225685a5fdb8f319bfc44d281fa5
RazYang <xzzorz@gmail.com>
2026-07-26 18:10:55 +0800

    Use lib.getExe for nixos-switch

--- packages/nixos-switch/package.nix
+++ packages/nixos-switch/package.nix
@@ -1,17 +1,15 @@
 {
+  lib,
   writeShellScriptBin,
   inputs,
   inputs',
   nixos-rebuild-ng,
   ...
 }:
-let
-  nixos-rebuild = "${nixos-rebuild-ng}/bin/nixos-rebuild-ng";
-in
 writeShellScriptBin "nixos-switch" ''
   if [ "$#" -gt 0 ]; then
-    exec ${nixos-rebuild} switch --flake "${inputs.self}#''${@}"
+    exec ${lib.getExe nixos-rebuild-ng} switch --flake "${inputs.self}#''${@}"
   else
-    exec ${nixos-rebuild} switch --flake "${inputs.self}"
+    exec ${lib.getExe nixos-rebuild-ng} switch --flake "${inputs.self}"
   fi
 ''
