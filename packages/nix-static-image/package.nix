1204bffaa490c5e6242c8c0101db71dff038caaf
RazYang <xzzorz@gmail.com>
2026-07-28 10:14:15 +0800

    Simplify static Nix image packaging

--- packages/nix-static-image/package.nix
+++ packages/nix-static-image/package.nix
@@ -1,68 +1,21 @@
 {
   cacert,
-  closureInfo,
   dockerTools,
   lib,
   nix-static,
   pkgsStatic,
-  writeTextDir,
   ...
 }:
 let
   busybox = pkgsStatic.busybox;
-  nixConfig = writeTextDir "etc/nix/nix.conf" (builtins.readFile ./nix.conf);
-  flakeRegistry = writeTextDir "etc/nix/flake-registry.json" (
-    builtins.toJSON {
-      version = 2;
-      flakes = [
-        {
-          from = {
-            type = "indirect";
-            id = "nixpkgs";
-          };
-          exact = true;
-          to = {
-            type = "tarball";
-            url = "https://channels.nixos.org/nixos-26.05/nixexprs.tar.zst";
-          };
-        }
-      ];
-    }
-  );
-  runtimeClosure = closureInfo {
-    rootPaths = [
-      cacert
-      busybox
-      nix-static
-    ];
-  };
 in
 dockerTools.streamLayeredImage {
-  name = "nix-static";
+  name = "nix";
   tag = "latest";
 
-  # Copy the validated runtime closure without adding a Nix database.
-  includeStorePaths = false;
-
   extraCommands = ''
-    mkdir -p etc/nix nix/store root tmp
-    cp ${nixConfig}/etc/nix/nix.conf etc/nix/nix.conf
-    cp ${flakeRegistry}/etc/nix/flake-registry.json etc/nix/flake-registry.json
-
-    while IFS= read -r storePath; do
-      case "$storePath" in
-        /nix/store/*-glibc-[0-9]*)
-          echo "unexpected glibc path in the image runtime closure: $storePath" >&2
-          exit 1
-          ;;
-        /nix/store/*musl-*-dev)
-          echo "unexpected musl development path in the image runtime closure: $storePath" >&2
-          exit 1
-          ;;
-      esac
-
-      cp -a "$storePath" nix/store/
-    done < ${runtimeClosure}/store-paths
+    mkdir -p etc/nix root tmp
+    cp -a ${./etc}/. etc/
   '';
 
   fakeRootCommands = ''
@@ -71,10 +24,8 @@ dockerTools.streamLayeredImage {
   '';
 
   config = {
-    Cmd = [
-      "nix"
-      "--version"
-    ];
+    Entrypoint = [ "nix" ];
+    Cmd = [ "--version" ];
     Env = [
       "HOME=/root"
       "USER=root"
